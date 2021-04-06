cdb9243b63cb2947b585673e7e17c64fae7beecb
90heum <792wjdgma@naver.com>
2021-04-06 15:01:29 +0900

    clickclick 완성되가는중

--- app/src/main/java/kr/co/heu_um/clickclickclick/MainActivity.java
+++ app/src/main/java/kr/co/heu_um/clickclickclick/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
@@ -21,13 +23,15 @@ import java.util.Collections;
 public class MainActivity extends AppCompatActivity {
 
     ImageView btn;
-    ImageView[] btnarr=new ImageView[12];
+    ImageView[] btnarr = new ImageView[12];
 
 
     ImageView btnfirst;
-    int imageIndex=0;
+    int imageIndex = 0;
+    TextView tv;
 
     private AdView mAdView;
+    Button reButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,43 +41,63 @@ public class MainActivity extends AppCompatActivity {
         // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
         MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
 
-        mAdView=findViewById(R.id.adView);
+        mAdView = findViewById(R.id.adView);
+        tv = findViewById(R.id.tv);
+        reButton = findViewById(R.id.btn_retry);
 
-        AdRequest adRequest= new AdRequest.Builder().build();
+        AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
 
+        gameSetting();
 
-        ArrayList<Integer> arr= new ArrayList<>();
-        for(int i=0; i<btnarr.length; i++){
+        btnfirst = (ImageView) findViewById(R.id.imageButton01);
+
+
+    }
+
+    public void clickBtn(View v) {
+
+        int tag = (int) v.getTag();
+        if (imageIndex == tag) {
+            v.setVisibility(View.INVISIBLE);
+            imageIndex++;
+            if (imageIndex >= 12) {
+                tv.setText("Clear");
+                reButton.setEnabled(true);
+            } else {
+                tv.setText("" + (imageIndex + 1));
+            }
+        }
+    }
+
+    public void clickBtn01(View v) {
+        btnfirst.setVisibility(View.INVISIBLE);
+    }
+
+    public void gameSetting(){
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < btnarr.length; i++) {
             arr.add(i);
         }
         Collections.shuffle(arr);
 
-        for(int i=0; i<btnarr.length; i++){
-            btnarr[i]=findViewById(R.id.btn01+i);
+        for (int i = 0; i < btnarr.length; i++) {
+            btnarr[i] = findViewById(R.id.btn01 + i);
+            btnarr[i].setImageResource(R.drawable.btn01 + arr.get(i));
             btnarr[i].setTag(arr.get(i));
         }
-
-
-
-
-
-
-        btnfirst=(ImageView)findViewById(R.id.imageButton01);
-
-
-    }
-
-    public void clickBtn(View v){
-        Button btn=(Button)v;
-    }
-    public void clickBtn01(View v){
-        btnfirst.setVisibility(View.INVISIBLE);
     }
 
 
-
-
-
+    public void clickRetry(View view) {
+//        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        gameSetting();
+        for (int i =0; i < btnarr.length; i++){
+            btnarr[i].setVisibility(View.VISIBLE);
+        }
+        imageIndex =0;
+        tv.setText("1");
+        reButton.setEnabled(false);
+    }
 }
